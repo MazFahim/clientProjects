97386b632e8d8f91a867a0d4a0c59b85ea57d6e2
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-03-27 11:45:05 +0600

    urls issue fixed and footer script added

--- Rupantor/management/urls.py
+++ Rupantor/management/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/test', views.test, name='test'),
+    path('test/', views.test, name='test'),
     path('', views.home, name='home'),
-    path('/summerwear', views.summerwear, name='summerwear'),
-    path('/winterwear', views.winterwear, name='winterwear'),
+    path('summerwear/', views.summerwear, name='summerwear'),
+    path('winterwear/', views.winterwear, name='winterwear'),
 ]
