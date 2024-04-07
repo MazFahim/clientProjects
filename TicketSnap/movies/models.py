de973bdb67445684c32eec3778388f9d3bc502a2
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-04-07 23:08:21 +0600

    Ticketsnap: test data inserted in a table

--- TicketSnap/movies/models.py
+++ TicketSnap/movies/models.py
@@ -17,6 +17,9 @@ class Movie(models.Model):
     status = models.CharField(max_length=20)
     trailer_url = models.URLField()
 
+    def __str__(self):
+        return f"{self.title} - {self.genre}"
+
 class Showtime(models.Model):
     Day_Choices = [
         ('Monday', 'Monday'),
