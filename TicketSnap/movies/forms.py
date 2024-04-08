842228be1c03859095feb6e0915dc322177d3d71
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-04-08 17:41:19 +0600

    Ticketsnap forms.py created

---
+++ TicketSnap/movies/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import CustomerMessage
+
+class CustomerMessageForm(forms.ModelForm):
+    class Meta:
+        model = CustomerMessage
+        fields = ['name', 'email', 'subject', 'msg']
