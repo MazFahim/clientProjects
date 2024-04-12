8c7f4154ff799aeade98d4ae3c5a8a9c5a33a6b3
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-04-12 12:03:33 +0600

    Rupantor: CustomerMessage Model Added

--- Rupantor/management/models.py
+++ Rupantor/management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Wears(models.Model):
+    pass
+
+class Cart(models.Model):
+    pass
+
+class CustomerMessage(models.Model):
+    msgId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    subject = models.CharField(max_length=20)
+    msg = models.TextField()
