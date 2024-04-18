78888851a14e7507898d560ab0140e4367177733
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-04-18 17:26:48 +0600

    TicketSnap: Image displaying tested

--- TicketSnap/TicketSnap/urls.py
+++ TicketSnap/TicketSnap/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('', include('movies.urls')),
     path('admin/', admin.site.urls),
-]
+] 
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
