e9d151910ac7a418654bc796ec1680bd7b4c6926
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-04-26 14:39:33 +0600

    Ticketsnap: signin, login created

--- TicketSnap/accounts/urls.py
+++ TicketSnap/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin, name='signin'),
-    path('signout/', views.signout, name='signout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- TicketSnap/accounts/views.py
+++ TicketSnap/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.contrib.auth import login, logout, authenticate
 
 
@@ -11,13 +11,27 @@ def signup(request):
         return redirect('/')
     else:
         form = RegisterForm()
-    # return render(request, 'accounts/signup.html', {'form': form})
     return render(request, 'accounts/signup.html', {'form': form})
 
-def signin(request):
-    pass
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'accounts/login.html', {'form': form, 'error': "Invalid username or password."})
+    
+    else:
+        form = LoginForm(request.POST)
+                
+    return render(request, 'accounts/login.html', {'form': form})
 
-def signout(request):
+def logout_view(request):
     logout(request)
     return redirect('login')
 
