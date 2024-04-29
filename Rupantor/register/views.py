3e30b1681461378267db9108d0e8893df70f7641
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-04-29 10:49:30 +0600

    Rupantor: Login/Logout added

--- Rupantor/register/views.py
+++ Rupantor/register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import login, logout, authenticate
 
 def register(request):
     if request.method == "POST":
@@ -10,3 +11,26 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'register/register.html', {"form":form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'accounts/login.html', {'form': form, 'error': "Invalid username or password."})
+    
+    else:
+        form = LoginForm(request.POST)
+                
+    return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
