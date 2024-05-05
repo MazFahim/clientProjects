4c486a5b16f10a7a5c921e8fef44308fa5ef80ae
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-05-05 10:54:00 +0600

    Rupantor:login, register, contact us template design modified

--- Rupantor/register/urls.py
+++ Rupantor/register/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
 ]
