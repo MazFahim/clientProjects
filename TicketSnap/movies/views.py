148980c7f7c5028dbc352ca5c7d66bc6e3d2e29c
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-05-06 07:36:48 +0600

    Ticketsnap: minor changes

--- TicketSnap/movies/views.py
+++ TicketSnap/movies/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from .models import *
 from .forms import CustomerMessageForm
 from django.db import transaction
+from django.contrib import messages
 
 def movies(request):
     template = loader.get_template('first.html')
@@ -42,11 +43,10 @@ def tickets(request):
             if Booking.objects.filter(bookingTime=showtime_mapper, seat__in=seat_ids, is_booked=True).exists():
                 return HttpResponse("One or more seats are already booked.", status=400)
 
-            # Check if this user has already booked any of these seats for this showtime
             if Booking.objects.filter(bookingTime=showtime_mapper, seat__in=seat_ids, user=request.user).exists():
                 return HttpResponse("You have already booked one or more of the selected seats for this showtime.", status=400)
 
-            # Create bookings for the selected seats
+            booked_seats = []
             for seat_id in seat_ids:
                 seat = Seat.objects.get(id=seat_id)
                 Booking.objects.create(
@@ -55,9 +55,11 @@ def tickets(request):
                     user=request.user,
                     is_booked=True
                 )
-        return redirect('/')  # Assuming you want to redirect to the home page on success
+                booked_seats.append(seat_id)
+
+            return redirect('/') 
     showtimes = ShowtimeMapper.objects.all().select_related('movie')
-    seats = Seat.objects.all()  # You might want to filter this if your seat list is too long
+    seats = Seat.objects.all()  
     context = {
         'showtimes': showtimes,
         'seats': seats,
