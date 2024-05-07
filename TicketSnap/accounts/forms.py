7a8f0e83c751e6cdb02a09bff6bf2fa5103df309
mazfahim <52876279+MazFahim@users.noreply.github.com>
2024-05-07 11:30:54 +0600

    Ticketsnap: Contact us template
    Modified

--- TicketSnap/accounts/forms.py
+++ TicketSnap/accounts/forms.py
@@ -9,7 +9,7 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-
+    
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
         self.fields['username'].help_text = None
--- TicketSnap/movies/forms.py
+++ TicketSnap/movies/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from .models import CustomerMessage
 
 class CustomerMessageForm(forms.ModelForm):
-    name = forms.TextInput()
-    email = forms.TextInput()
-    subject = forms.TextInput()
-    msg = forms.TextInput()
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    subject = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    msg = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4}))
+
     class Meta:
         model = CustomerMessage
         fields = ['name', 'email', 'subject', 'msg']
